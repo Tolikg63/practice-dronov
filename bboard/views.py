fb77dbad3fc04076aea1113c3d634e459cd874ca
Tolik_mac <ttserunyan@inbox.ru>
2025-09-24 12:36:40 +0400

    Add template

--- bboard/views.py
+++ bboard/views.py
@@ -1,7 +1,11 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Bb
 
 
 def index(request):
-   return HttpResponse("The news are coming!!!")
-
-
+   # bbs = Bb.objects.order_by('-publish')
+   data = {
+      'list': Bb.objects.order_by('-publish')
+   }
+   return render(request, 'bboard/index.html', context=data)

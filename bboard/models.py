cd8f52c6417d77e8dc7b55a9a6678ff9033558f0
Tolik_mac <ttserunyan@inbox.ru>
2025-09-24 16:01:07 +0400

    Added rubrics feature

--- bboard/models.py
+++ bboard/models.py
@@ -1,18 +1,27 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Bb(models.Model):
    title = models.CharField(max_length=50, verbose_name='Товар')
    content = models.TextField(verbose_name='Описание')
    price = models.FloatField(verbose_name='Цена')
-   publish = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Опубликовано')
-
-
-   def __str__(self):
-      return self.title
-   
+   publish = models.DateTimeField(default=timezone.now, db_index=True, verbose_name='Опубликовано')
+   rubric = models.ForeignKey('Rubric', null=True, on_delete=models.PROTECT, verbose_name='Рубрика')
 
    class Meta:
       verbose_name_plural = 'Объявления'
       verbose_name = 'Объявление'
       ordering = ['-publish']
+
+
+class Rubric(models.Model):
+   name = models.CharField(max_length=20, db_index=True, verbose_name='Название')
+
+   class Meta:
+      verbose_name_plural = 'Рубрики'
+      verbose_name = 'Рубрика'
+      ordering = ['name']
+
+   def __str__(self):
+      return self.name
